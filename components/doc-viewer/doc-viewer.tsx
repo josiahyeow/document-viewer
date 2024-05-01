db668ecaae4c66772444e8cbf72d7d2591e2d12a
Josiah Yeow <josiah.yeow@fabricgroup.com.au>
2024-05-01 11:11:07 +1000

    feat: add doc viewer

---
+++ components/doc-viewer/doc-viewer.tsx
@@ -0,0 +1,24 @@
+import ReactDocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+
+export function DocViewer() {
+  const docs = [
+    {
+      uri: "https://doc-viewer-spike-documents.s3.ap-southeast-2.amazonaws.com/gov-id-requirements.pdf",
+    },
+
+    {
+      uri: "https://doc-viewer-spike-documents.s3.ap-southeast-2.amazonaws.com/file_example_XLSX_100.xlsx",
+    },
+    {
+      uri: "https://doc-viewer-spike-documents.s3.ap-southeast-2.amazonaws.com/file-sample_500kB.docx",
+    },
+  ];
+
+  return (
+    <ReactDocViewer
+      documents={docs}
+      pluginRenderers={DocViewerRenderers}
+      style={{ height: "80dvh" }}
+    />
+  );
+}
